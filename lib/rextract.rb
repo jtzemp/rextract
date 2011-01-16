dd653fd3492940f3ccd72255dc9190d260ad17b4
JT Zemp <jtzemp@gmail.com>
2011-01-15 23:42:58 -0700

    added the Rextract module guts and got the spec_helper.rb including the right file(s). Also changed a default failing spec.

---
+++ lib/rextract.rb
@@ -0,0 +1,5 @@
+module Rextract
+  autoload :Browser, 'rextract/browser'
+  autoload :Spider,  'rextract/spider'
+  autoload :Parser,  'rextract/parser'
+end
--- spec/rextract_spec.rb
+++ spec/rextract_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Rextract" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "doesn't fail" do
+    true.should == true
   end
 end
