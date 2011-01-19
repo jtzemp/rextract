f3fde9da06393e232d40679cebb40819b334985a
JT Zemp <jtzemp@gmail.com>
2011-01-19 00:13:51 -0700

    Added basic parser functionality.

---
+++ lib/rextract/parser.rb
@@ -0,0 +1,26 @@
+require 'rextract'
+require 'nokogiri'
+
+class Rextract::Parser
+  def initialize(content)
+    @content = content
+    @doc = Nokogiri::HTML(content)
+  end
+
+  def extract(regex)
+    @content.scan(regex).flatten.first
+  end
+
+  def parsing_methods
+    (self.methods - self.class.superclass.methods).collect{|m| m if m =~ /parse_/}.compact
+  end
+    
+  def parse
+    result = {}
+    parsing_methods.each do |method|
+      key = method.scan(/parse_(\w*)/).flatten.first.to_sym
+      result[key] = self.send(method.to_sym)
+    end
+    result
+  end
+end
---
+++ spec/rextract/parser_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Rxtract::Parser do
+  
+end
