734c04a78fc3222145858a70b101eb0209651858
JT Zemp <jtzemp@gmail.com>
2011-01-19 22:59:37 -0700

    All valid Mechanize accessors can get set through the options to Rextract::Spider.new(options)

--- lib/rextract/spider.rb
+++ lib/rextract/spider.rb
@@ -4,12 +4,11 @@ class Rextract::Spider
   attr_reader :agent
   def initialize(opts = {})
     @agent = Rextract::Browser.new do |a|
-      if opts.is_a?(Hash)
-        a.user_agent_alias    = opts[:ua_alias] || 'Mac Safari'
-        a.request_headers     = opts[:request_headers] if opts.has_key?(:request_headers)
-        a.follow_meta_refresh = true
-      else
-        a.user_agent_alias    = 'Mac Safari'
+      a.user_agent_alias    = opts[:user_agent_alias] || 'Mac Safari'
+      a.follow_meta_refresh = true
+      opts.each do |key, val|
+        method = "#{key}="
+        a.send(method, val) if a.respond_to?(method)
       end
     end
   end
--- spec/rextract/spider_spec.rb
+++ spec/rextract/spider_spec.rb
@@ -2,15 +2,29 @@ require 'spec_helper'
 
 describe Rextract::Spider do
   describe "#new" do
-    describe "options hash" do
-      it "accepts :ua_alias and passes it to the Mechanize agent" do
-        spider = Rextract::Spider.new(:ua_alias => "Mac FireFox")
-        spider.agent.user_agent.should == "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6"
+    describe "options hash tries to set all options on the Mechanize agent" do
+      describe ":user_agent_alias" do
+        it "accepts :ua_alias and passes it to the Mechanize agent" do
+          spider = Rextract::Spider.new(:user_agent_alias => "Mac FireFox")
+          spider.agent.user_agent.should == "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6"
+        end
+        
+        it ":ua_alias defaults to 'Mac Safari'" do
+          spider = Rextract::Spider.new
+          spider.agent.user_agent.should == "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_2; de-at) AppleWebKit/531.21.8 (KHTML, like Gecko) Version/4.0.4 Safari/531.21.10"
+        end
       end
       
-      it ":ua_alias defaults to 'Mac Safari'" do
-        spider = Rextract::Spider.new
-        spider.agent.user_agent.should == "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_2; de-at) AppleWebKit/531.21.8 (KHTML, like Gecko) Version/4.0.4 Safari/531.21.10"
+      describe ":request_headers" do
+        it "accepts :request_headers as a Hash" do
+          spider = Rextract::Spider.new(:request_headers => {"Funky" => "Chicken"})
+          spider.agent.request_headers.should == {"Funky" => "Chicken"}
+        end
+        
+        it "defaults to none" do
+          spider = Rextract::Spider.new
+          spider.agent.request_headers.should == {}
+        end
       end
     end
   end
