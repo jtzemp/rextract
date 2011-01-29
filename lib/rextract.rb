6d32a53e769defb994c923532f4772b81eaa65d0
JT Zemp <jtzemp@gmail.com>
2011-01-28 23:14:04 -0700

    Read the VERSION file to set the module version.

--- lib/rextract.rb
+++ lib/rextract.rb
@@ -2,5 +2,5 @@ module Rextract
   require 'rextract/browser'
   require 'rextract/spider'
   require 'rextract/parser'
-  VERSION = "0.1.3"
+  VERSION = File.open(File.expand_path(File.dirname(__FILE__)) + "/../VERSION").read
 end
